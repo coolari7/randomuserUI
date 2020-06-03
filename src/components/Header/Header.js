3d6b239e8457e49988e9f54d504c5d029ae017e0
Arijit <coolari7@gmail.com>
2020-06-03 21:16:03 +0530

    Fixed <a> bug

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,13 @@ import logo from '../../assets/logo.png'
 
 const Header = (props) => (
     <nav className="header__wrapper">
-        <a  href="/">
+        <a  href="#">
             <img src={logo} alt="logo" />
         </a>
         <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Contact</a></li>
-            <li><a href="/">About</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About</a></li>
         </ul>
         <a href="https://randomuser.me/">randomuser.me</a>
     </nav>
