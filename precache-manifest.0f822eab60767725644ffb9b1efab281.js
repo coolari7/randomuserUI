1551a8bf3b1a7475b9795c1cf0fbc9741a8613a9
Arijit <coolari7@gmail.com>
2020-06-03 20:22:04 +0530

    Updates

---
+++ precache-manifest.0f822eab60767725644ffb9b1efab281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e95d454aee8432e8037c242c9cf75d",
+    "url": "/randomuserUI/index.html"
+  },
+  {
+    "revision": "6775717413b9bb245888",
+    "url": "/randomuserUI/static/css/main.29806eb5.chunk.css"
+  },
+  {
+    "revision": "3aab9f6dcc965b7ceab9",
+    "url": "/randomuserUI/static/js/2.9c55b430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/randomuserUI/static/js/2.9c55b430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6775717413b9bb245888",
+    "url": "/randomuserUI/static/js/main.6d34b068.chunk.js"
+  },
+  {
+    "revision": "1d6fc2559bb73dcb82bf",
+    "url": "/randomuserUI/static/js/runtime-main.50916fa7.js"
+  }
+]);
